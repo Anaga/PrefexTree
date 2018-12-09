bf2e04b0cdeeba0a32c6dc83f351179edaf76d89
Aleksandr Bobyrev <bobyrev.bob@gmail.com>
2018-12-09 23:10:14 +0200

    Big rework to swith on std
    Fix most function signatures,
    Add helper functions isLetter and toLower

--- main.cpp
+++ main.cpp
@@ -4,37 +4,19 @@
 #include <QTextStream>
 #include <QDebug>
 #include <QString>
-#include <iostream>
 #include <QFile>
 #include <QDateTime>
 #include <QMap>
 #include <QLocale>
 
+#include <iostream>
+#include <map>
+#include <string>
+
 using namespace std;
 
 #define N 20
 #define MIN_WORD_LEN 4
-
-bool checkFile(QString qsFileName){
-    QString message;
-
-    QFile file(qsFileName);
-    if (!file.exists()){
-        message = "Can't find file %1 to read\n";
-        message = message.arg(qsFileName);
-        cout <<  qPrintable(message);
-        return false;
-    }
-
-    if(!file.open(QFile::ReadOnly | QFile::Text))  {
-        message = "Can't open the file %1 for reading\n";
-        message = message.arg(qsFileName);
-        cout <<  qPrintable(message);
-        return false;
-    }
-    file.close();
-    return true;
-}
 
 void generateWords(pair<long, string> item, QMap<QString, long> *m){
 
@@ -59,59 +41,82 @@ void generateWords(pair<long, string> item, QMap<QString, long> *m){
     }
 }
 
-QString readFile(QString qsFileName, QMap<QString, long> *m){
-    QString word;
-    QChar ch;
-    //char old_ch;
+bool isLetter(char ch){
+    if ((ch>='a') && (ch<='z')) return true;
+    if ((ch>='A') && (ch<='Z')) return true;
+    return false;
+}
+
+string toLower(string s){
+    char ch;
+    string lower;
+    char delta = 'a'-'A';
+    for (size_t i=0; i<s.length(); i++){
+        ch = s.at(i);
+        if ((ch>='A') && (ch<='Z')) ch+=delta; //up from capital letter to lower letter (char delta - 32)
+        lower.push_back(ch);
+    }
+    return lower;
+}
+string readFile(string qsFileName, map<string, long> *m){
+    string word;
+    char ch;
+
     bool startWord = false;
     long count=0;
     long longWords = 0;
     long unicWords = 0;
+    FILE *file;
+    string message;
 
-    QFile file(qsFileName);
-    file.open(QFile::ReadOnly | QFile::Text);
+    map<string, long>::iterator it;
 
-    QTextStream inputConsole(&file);
-    inputConsole.setAutoDetectUnicode(true);
-    inputConsole.setCodec("UTF-8");
-    while (!inputConsole.atEnd()) {
-        inputConsole >> ch;
-        if (ch.isLetter() && (!startWord)){
+    file = fopen(qsFileName.data(), "r");
+
+    if (file == nullptr) {
+        message = "Can't open file ";
+        cout << message << qsFileName << endl;
+        return message;
+    }
+    while ( ! feof(file) ) {
+        ch =static_cast<char>( fgetc(file) );
+        //cout << ch;
+        if (isLetter(ch) && (!startWord)){
             //we start to collect new word
             startWord = true;
             word = "";
         }
-        if (ch.isLetter() && (startWord)){
+        if (isLetter(ch) && (startWord)){
             //we continue to collect word
-            word.append(ch);
+            word.push_back(ch);
         }
-        if (!ch.isLetter() && (startWord)){
-            // qDebug() << word;
+        if (!isLetter(ch) && (startWord)){
+            //cout << word << endl;
             count++;
             if (word.length()>=MIN_WORD_LEN) {
-                QString lowerWord = word.toLower();
+                string lowerWord = toLower(word);
                 long count = 1;
-                if (!m->contains(lowerWord)){
-                    unicWords++;
-                }else {
-                    count = m->value(lowerWord);
-                    count++;
-                }
-                m->insert(lowerWord,count);
-                //qDebug() << "insert " << lowerWord << " with value " << count;
+                it = m->find(lowerWord);
 
-                //generateWords(word, m);
+                if (it == m->end()){
+                    unicWords++;
+                    m->emplace(lowerWord,count);
+                }else {
+                    it->second++;
+                    count = it->second;
+                }
+                //cout << "insert '" << lowerWord << "' with value " << count << endl;
                 longWords++;
-                // qDebug() << word;
             }
             //stop collect word
             startWord = false;
         }
     }
+    fclose(file);
+    cout << "Ok, in file " << qsFileName << ", all word count is " << count <<
+            ", long word is " << longWords << " count, unique long words is " <<unicWords << endl;
 
-    QString message = "Ok, in file %1, all word count is %2, long word is %3 count, unique long words is %4\n";
-    message = message.arg(qsFileName).arg(count).arg(longWords).arg(unicWords);
-
+    message = "Ok";
     return message;
 }
 
@@ -137,14 +142,14 @@ QMap<QString, long> generateExtraMap(QMap<QString, long> originalMap){
     return newMap;
 }
 
-long fillVector(vector<pair<long, string>> *toFill, QMap<QString, long> fromMap){
+long fillVector(vector<pair<long, string>> *toFill, map<string, long> fromMap){
     long total=0;
     pair<long, string> myItem;
-    QMap<QString, long>::const_iterator i=fromMap.begin();
+    auto i=fromMap.begin();
     while (i!=fromMap.end()) {
-        total+= i.value();
-        myItem.first = i.value();
-        myItem.second = qPrintable(i.key());
+        total+= i->second;
+        myItem.first = i->second;
+        myItem.second = i->first;
         toFill->push_back(myItem);
         ++i;
     }
@@ -161,89 +166,55 @@ void printVector(vector<pair<long, string>> toPrint, long totalCount, int topCou
     for (auto myItem : toPrint){
         intVal = myItem.first;
         percent = static_cast<double>(intVal*100) / static_cast<double>(totalCount);
-        cout << myItem.second << "\t : " << percent << "% \t: ("<<intVal<< ") of " << totalCount << endl;
+        if (myItem.second.length()<8) {
+            cout << myItem.second << "\t\t : " << percent << "% \t: ("<<intVal<< ") of " << totalCount << endl;
+        } else {
+            cout << myItem.second << "\t : " << percent << "% \t: ("<<intVal<< ") of " << totalCount << endl;
+        }
         cur++;
         if (cur>=topCount) break;
     }
 }
 
-void analyseLog(QString fileName){
-    QDateTime qdtStart = QDateTime::currentDateTime();
-    qint64 startMilSec = QDateTime::currentMSecsSinceEpoch();
-    QFile file(fileName);
-
-    qint64 iFileSize = file.size();
-    qint64 iTimeOut = iFileSize/10000000;
-    QString message = "File size %1 in bytes\nTo parse it, this can take %2 sec \n";
-    message = message.arg(iFileSize).arg(iTimeOut);
-    cout <<  qPrintable(message);
-
-    if (!checkFile(fileName)){
-        cout << "Can't handl file" << qPrintable(fileName);
-        return;
-    }
-
-    QMap<QString, long> myMap;
+void analyseLog(string fileName){
+    string message;
+    map<string, long> myMap;
+    cout << "start to read file " << fileName << endl;
     message = readFile(fileName, &myMap);
-    cout << qPrintable(message );
-
-    QDateTime qdtStop = QDateTime::currentDateTime();
-    qint64 stpoMilSec = QDateTime::currentMSecsSinceEpoch();
-    qint64 delta = stpoMilSec - startMilSec;
-    message = "Start time %1\n"
-              "Stop  time %2\n"
-              "Delta %3 milsec\n";
-    QString qsTimeFormat = "hh:mm:ss.z";
-    message = message.arg(qdtStart.toString(qsTimeFormat)).arg(qdtStop.toString(qsTimeFormat)).arg(delta);
-    cout << qPrintable(message);
+    cout << message;
 
     message = "Start to generate extended map, with new short words.\n";
-    cout << qPrintable(message);
+    cout << message;
 
-    QMap<QString, long> extendedMap = generateExtraMap(myMap);
-    message = "Extended map now have %1 elements\n";
-    message =  message.arg(extendedMap.count());
-    cout << qPrintable(message);
+    map<string, long> extendedMap; // = generateExtraMap(myMap);
+    message = "Extended map now have";
+    cout << message << extendedMap.size() << "elements\n";
 
     message = "Fill vector from extended map.\n";
-    cout << qPrintable(message);
+    cout << message;
 
     long totalCount=0;
     vector<pair<long, string>> myVector;
-    totalCount = fillVector(&myVector,extendedMap);
+    totalCount = fillVector(&myVector,myMap);
 
     message = "Print top N words and it percents. N is ";
-    cout << qPrintable(message) << N << endl;
+    cout << message << N << endl;
     printVector(myVector, totalCount, N);
 
 }
 
-int main(int argc, char *argv[])
+int main()
 {
-    QCoreApplication a(argc, argv);
-    QString qsFileName = "s2.txt";
+    string qsFileName = "s2.txt";
     analyseLog(qsFileName);
+
 
     qsFileName = "s.txt";
     analyseLog(qsFileName);
 
-    qsFileName = "Simple.txt";
-    analyseLog(qsFileName);
-
     qsFileName = "QChar.md";
     analyseLog(qsFileName);
-
-    qsFileName = "timing-4.log";
-  //  analyseLog(qsFileName);
-
-    qsFileName = "QtWebEngine5.12.html";
-    analyseLog(qsFileName);
-
-    qsFileName ="THREE-MEN-IN-A-BOAT.html";
-  //  analyseLog(qsFileName);
-
-    qsFileName = "jsonfile.log";
-   // analyseLog(qsFileName);
-
-    return a.exec();
+/*
+*/
+    return 0;
 }
